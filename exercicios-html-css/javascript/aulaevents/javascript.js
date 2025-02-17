543da42f55f5054c35a517dc47d6127aeb9150a8
Vitor Martiniano <vitormwxm@gmail.com>
2025-02-17 13:42:37 -0300

    outros eventos

--- exercicios-html-css/javascript/aulaevents/javascript.js
+++ exercicios-html-css/javascript/aulaevents/javascript.js
@@ -7,3 +7,14 @@ function mostrar() {
 }
 
 button.addEventListener("click" , mostrar);
+
+const larguraTela = window.innerWidth;
+function coordenadasMouse(event){
+    const coordenadas = {
+        x: event.x,
+        y: event.y,
+    }; 
+    console.log(coordenadas)
+}
+
+window.addEventListener("mousemove" , coordenadasMouse)
