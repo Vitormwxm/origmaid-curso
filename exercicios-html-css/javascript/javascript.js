3a2a7c652c5e5eba20fe463e121f7438c26f56e1
Vitor Martiniano <vitormwxm@gmail.com>
2025-02-17 13:16:51 -0300

    exercicio javascript Origamid

--- exercicios-html-css/javascript/javascript.js
+++ exercicios-html-css/javascript/javascript.js
@@ -1,7 +1,19 @@
-const livro = document.querySelector('ul li');
 
-const livroConhecido = {
-    livroId: livro
-};
 
-console.log(livroConhecido.livroId)
+function livro(nome , ano , autor) {
+    this.nome = nome.toUpperCase();
+    this.ano = 2050 - ano;
+    this.autor = autor;
+
+    const frase = this.nome + 'por' + this.autor;
+
+    return objectLivro = {
+        nome: this.nome,
+        ano: this.ano,
+        autor: this.autor,
+    }
+}
+
+retorno = livro('O senhor dos Aneis' , 1954 , 'J. R. R. Tolkien');
+console.log(retorno);
+console.log()
