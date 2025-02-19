6a1f2c3118a435a6d8b9c47538d7af065a0efa18
Vitor Martiniano <vitormwxm@gmail.com>
2025-02-18 23:24:05 -0300

    exercicio1107 - origamid corrigido

--- exercicios-html-css/javascript/exercicio1107/javascript.js
+++ exercicios-html-css/javascript/exercicio1107/javascript.js
@@ -4,9 +4,15 @@ function somar (){
     let div = document.querySelector("div");
     let valor = div.textContent;
 
-    let soma = parseInt(valor) + 1 ;
+   
 
-    div.innerHTML = soma
+    if(valor < 10) {
+        let soma = parseInt(valor) + 1 ;
+
+        div.innerHTML = soma
+    } else {
+        console.log("não é possível mais adicionar")
+    }
 };
 
 if (botao) {
