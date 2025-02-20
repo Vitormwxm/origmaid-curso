55aa9ab3dcd03376ebb48f19f6c599a623b9daa3
Vitor Martiniano <vitormwxm@gmail.com>
2025-02-20 11:01:31 -0300

    utilizando javascript e adicionando plugins

--- exercicios-html-css/projeto-final/javascript/javascript.js
+++ exercicios-html-css/projeto-final/javascript/javascript.js
@@ -10,3 +10,46 @@ function linkAtivar(link) {
 }
 
 links.forEach(linkAtivar)
+
+//
+
+const perguntas = document.querySelectorAll(".perguntas-list button")
+console.log(perguntas)
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa")
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded" , ativa)
+    console.log(controls);
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click" , ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas);
+
+//bicicletas
+
+const galeria = document.querySelectorAll(".bicicletas-imagem img")
+const galeriaContainer = document.querySelector(".bicicletas-imagem");
+
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    galeriaContainer.prepend(img)
+}
+
+function eventosGaleria(img) {
+    img.addEventListener("click", trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
+
+if(window.SimpleAnime){
+    new SimpleAnime();
+}
