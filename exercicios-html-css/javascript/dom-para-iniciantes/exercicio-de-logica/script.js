15b64d570a484f1d984c51034bfa07bf682ead9f
Vitor Martiniano <vitormwxm@gmail.com>
2025-03-21 11:23:01 -0300

    criando arquivos de logica

---
+++ exercicios-html-css/javascript/dom-para-iniciantes/exercicio-de-logica/script.js
@@ -0,0 +1,26 @@
+
+const menu = document.querySelector(".menu");
+
+// calcula o tamanho do elemento , altura e padding 
+console.log("client Height " + menu.clientHeight);
+
+// calcula o tamanho do elemento levando em consideração, altura , padding e border
+console.log("offsetHeight " + menu.offsetHeight);
+
+// calcula o tamanho do elemento mesmo dentro de um scroll 
+console.log("scrollHeight " + menu.scrollHeight);
+
+
+// calcula a distância do elemento até o topo da página;
+console.log("offsetTop " + menu.offsetTop );
+
+// cacula a distâcia do elemento até seu lado esquerdo na página
+console.log("offsetLeft " + menu.offsetLeft);
+
+function aumetarDistancia(elemento, valor ) {
+    let menuRect = elemento.getBoundingClientRect();
+    let distancia = menuRect.height + valor;
+    elemento.style.setProperty("padding-top" , distancia + "px")
+};
+
+aumetarDistancia(menu , 350);
