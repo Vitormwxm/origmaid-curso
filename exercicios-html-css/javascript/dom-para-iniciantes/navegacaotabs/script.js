a4310a51cd709a76a258bebb006916727bae501e
Vitor Martiniano <vitormwxm@gmail.com>
2025-04-02 11:54:35 -0300

    accordion list

--- exercicios-html-css/javascript/dom-para-iniciantes/navegacaotabs/script.js
+++ exercicios-html-css/javascript/dom-para-iniciantes/navegacaotabs/script.js
@@ -1,16 +1,22 @@
+
+function initNavTab(){
 // estou fazendo uma navegação por tabs, onde vou clicar em uma imagem e ela vai mostra 
 // o conteudo
-
-const tabMenu = document.querySelectorAll(".js-tabImagem li");
-const tabContent = document.querySelectorAll(".js-tabContent section");
+    const tabMenu = document.querySelectorAll(".js-tabImagem li");
+    const tabContent = document.querySelectorAll(".js-tabContent section");
 
 // essa função vai adicionar a classe ativo na section passada no index. 
 // essa função também remove todas os tabContents com ativo na section
-function activeTab (index) {
-    tabContent.forEach((section) => {
-        section.classList.remove("ativo")
-    })
-    tabContent[index].classList.add("ativo")
+
+if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo")
+
+    function activeTab (index) {
+        tabContent.forEach((section) => {
+            section.classList.remove("ativo")
+        })
+        tabContent[index].classList.add("ativo")
+    }
 }
 
 // ao clicar no elemento ele vai executar o addEvent e chamar a função activeTab passando o index
@@ -19,3 +25,26 @@ tabMenu.forEach((itemMenu, index) => {
         activeTab(index)
     })
 })
+}
+
+
+function initAcordionList() {
+    const titleAccordion = document.querySelectorAll(".js-accordionList dt")
+    if(titleAccordion.length) {
+        titleAccordion[0].classList.add("ativo")
+    titleAccordion[0].nextElementSibling.classList.add("ativo")
+
+    function activeAccordion() {
+        this.classList.toggle("ativo")
+        this.nextElementSibling.classList.toggle("ativo")
+        
+    }
+
+    titleAccordion.forEach((item) => {
+        item.addEventListener('click' , activeAccordion)
+    })
+    }
+
+}   
+initNavTab();
+initAcordionList();
