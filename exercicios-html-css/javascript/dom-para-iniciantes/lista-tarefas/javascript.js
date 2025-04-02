836e1d84212dc01a74f4e4162817ab4efbb2e65a
Vitor Martiniano <vitormwxm@gmail.com>
2025-04-01 22:11:41 -0300

    caixa de entrada projeto para estudar

---
+++ exercicios-html-css/javascript/dom-para-iniciantes/lista-tarefas/javascript.js
@@ -0,0 +1,16 @@
+const input = document.querySelector("#addtask")
+const lista = document.querySelector(".lista");
+
+function valorInput (Event) {
+    let valorInput = input.value
+    if(Event.key === "Enter") {
+        let novoItem = document.createElement("li")
+        novoItem.textContent = valorInput
+        lista.appendChild(novoItem)
+
+        input.value = "";
+    }
+}
+
+
+input.addEventListener("keydown", valorInput)
